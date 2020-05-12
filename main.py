328e0fae51f6f78731d27839682f1c264070e5a7
Nikita Gorokhov <wackloner@yandex-team.ru>
2020-05-12 04:54:14 +0300

    use start_webhook function

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import logging
 import os
 
-from aiohttp import web
+from aiogram.utils.executor import start_webhook
 from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher.webhook import get_new_configured_app
 
 API_TOKEN = '1221172107:AAG7GK7OBKMXLASZpbb4fME_SztgOoECF6o'
 PROJECT_NAME = 'moshnar-bot'
 
-PORT = os.getenv('PORT') or 8080
+APP_HOST = "localhost"
+APP_PORT = os.getenv('PORT') or 8080
 # PROXY_URL = 'http://167.71.105.68:8080'
 
 WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com/'
-WEBHOOK_URL_PATH = f'/webhook/{API_TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_URL_PATH}'
+WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 logging.basicConfig(level=logging.INFO)
 
@@ -36,13 +36,28 @@ async def echo(message: types.Message):
     await message.answer(f'{message.text} хули')
 
 
-async def on_startup(app):
-    await bot.delete_webhook()
+async def on_startup(dp):
+    logging.info('Starting...')
+
     logging.info(f'Setting webhook to {WEBHOOK_URL}')
     await bot.set_webhook(WEBHOOK_URL)
 
 
+async def on_shutdown(dp):
+    logging.info('Shutting down...')
+
+    await bot.delete_webhook()
+
+    logging.info('Bye!')
+
+
 if __name__ == '__main__':
-    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
-    app.on_startup.append(on_startup)
-    web.run_app(app, host='0.0.0.0', port=PORT)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=APP_HOST,
+        port=APP_PORT,
+    )
