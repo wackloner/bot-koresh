89f2c65857bcdc27686df5991d44025a13051a7a
Nikita Gorokhov <wackloner@gmail.com>
2020-06-10 17:36:41 +0300

    refactor challenge

--- bot/commands/button_handler.py
+++ bot/commands/button_handler.py
@@ -14,36 +14,41 @@ def button_handler(update: Update, context: CallbackContext):
         query.answer()
         address = query.data
 
-        if address == 'ЖМЯК':
-            update.callback_query.edit_message_text(f'Ии первым был @{query.from_user.username} (:')
-            return
-
+        challenge_id = -1
         try:
-            # TODO: fix bad code smell of using exceptions as conditions
             challenge_id = int(address)
-            player = f'@{query.from_user.username}'
-
-            # TODO: add spent duration info
-            if 'challenge_result_msg' not in context.chat_data:
-                context.chat_data['challenge_result_msg'] = {}
-            if challenge_id not in context.chat_data['challenge_result_msg']:
-                res_msg = context.bot.send_message(context.chat_data['id'], player)
-                context.chat_data['challenge_result_msg'][challenge_id] = res_msg.message_id
-
-                # TODO: use default dict
-                if 'challenge_results' not in context.chat_data:
-                    context.chat_data['challenge_results'] = {}
-                if challenge_id not in context.chat_data['challenge_results']:
-                    context.chat_data['challenge_results'][challenge_id] = []
-
-            context.chat_data['challenge_results'].append(player)
-
-            res_msg = context.chat_data['challenge_result_msg'][challenge_id]
-            text = '\n'.join(context.chat_data['challenge_results'])
-            context.bot.edit_message_text(chat_id=context.chat_data['id'], message_id=res_msg, text=text)
-
         except Exception:
             pass
+
+        if challenge_id != -1:
+            try:
+                player = f'@{query.from_user.username}'
+                logging.debug(f'{player} in A GAME yo')
+
+                # TODO: add spent duration info
+                if 'challenge_result_msg' not in context.chat_data:
+                    context.chat_data['challenge_result_msg'] = {}
+                if challenge_id not in context.chat_data['challenge_result_msg']:
+                    res_msg = context.bot.send_message(context.chat_data['id'], player)
+                    context.chat_data['challenge_result_msg'][challenge_id] = res_msg.message_id
+
+                    # TODO: use default dict
+                    if 'challenge_results' not in context.chat_data:
+                        context.chat_data['challenge_results'] = {}
+                    if challenge_id not in context.chat_data['challenge_results']:
+                        context.chat_data['challenge_results'][challenge_id] = []
+
+                context.chat_data['challenge_results'][challenge_id].append(player)
+
+                res_msg = context.chat_data['challenge_result_msg'][challenge_id]
+                text = '\n'.join(context.chat_data['challenge_results'][challenge_id])
+                context.bot.edit_message_text(chat_id=context.chat_data['id'], message_id=res_msg, text=text)
+
+                return
+
+            except Exception as e:
+                logging.error(e)
+                return
 
         tracking = app_context.tracking_manager.get_tracking_by_address(address)
 
